072f377ff37604143aeac71714b83576fac15e58
conspir0 <michalczyk.mat3usz@gmail.com>
2020-08-29 22:54:31 +0200

    Create class App, func.comp. ButtonElement, BasketQty.

--- App.js
+++ App.js
@@ -1,15 +1,77 @@
 class App extends React.Component {
-  state = {}
+  state = {
+    avaliableItems: 10,
+    qty: 0
+  }
+
+  buttonHandler = (type) => {
+    if (type === 'add' && this.state.avaliableItems > 0) {
+      this.setState((prevState) => ({
+        avaliableItems: prevState.avaliableItems - 1,
+        qty: prevState.qty + 1
+      }))
+    } else if (type === 'remove' && this.state.qty > 0) {
+      this.setState((prevState) => ({
+        avaliableItems: prevState.avaliableItems + 1,
+        qty: prevState.qty - 1
+      }))
+    } else if (type === 'submit' && this.state.qty > 0) {
+      this.setState((prevState) => ({
+        qty: 0
+      }))
+    }
+  }
 
   render() {
+    const { qty, avaliableItems } = this.state;
+
     return (
       <React.Fragment>
         <div className="container">
-          Hello World!
+          <div className="button-container">
+            <ButtonElement
+              title="+"
+              buttonEvent={() => this.buttonHandler('add')}
+              isDisabled={avaliableItems > 0 ? false : true}
+            />
+            <BasketQty qty={qty} />
+            <ButtonElement
+              title="-"
+              buttonEvent={() => this.buttonHandler('remove')}
+              isDisabled={qty > 0 ? false : true}
+            />
+            <ButtonElement
+              title="buy"
+              buttonEvent={() => this.buttonHandler('submit')}
+              isDisabled={qty > 0 ? false : true}
+            />
+          </div>
         </div>
       </React.Fragment>
     )
   }
+}
+
+const ButtonElement = ({ title, isDisabled, buttonEvent }) => {
+  return (
+    <button
+      onClick={buttonEvent}
+      disabled={isDisabled}
+      className="button"
+    >
+      {title}
+    </button>
+  )
+}
+
+const BasketQty = ({ qty }) => {
+  return (
+    <div
+      className={`result-box ${qty === 0 ? 'transparent' : ''}`}
+    >
+      {qty}
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
